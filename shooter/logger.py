32a7fdc3bbbbc31cac2979fa3d60ce8618593649
Alex Ehlke <alex.ehlke@gmail.com>
2012-01-28 00:23:53 -0500

    raise logger level

--- shooter/logger.py
+++ shooter/logger.py
@@ -3,7 +3,7 @@ import logging
 
 def logger(name):
     log = logging.getLogger(name)
-    log.setLevel(logging.INFO)
+    log.setLevel(logging.WARNING)
     formatter = logging.Formatter('[%(levelname)s:%(name)s] %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
