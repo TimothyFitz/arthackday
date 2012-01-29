487584596a20155699a2466111f94324b8beb5ef
Timothy Fitz <TimothyFitz@gmail.com>
2012-01-28 19:23:49 -0500

    All start buttons start.

--- main.py
+++ main.py
@@ -380,7 +380,7 @@ def main():
             
             player.move(dx, dy)
 
-            if start_screen_visible() and (keys[pygame.K_RETURN] or joy.state.buttons and joy.state.buttons[7]) and (player.health <= 0 or boss.health <= 0):
+            if start_screen_visible() and (keys[pygame.K_RETURN] or joy.state.buttons and (joy.state.buttons[7] or joy.state.buttons[6] or joy.state.buttons[4] or joy.state.buttons[5]) and (player.health <= 0 or boss.health <= 0)):
                 enemy_bullets.clear()
                 player.health = 100.
                 boss.health = 100.
